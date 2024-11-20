7f1eb8721c04ff42552f13742adc4919c7fa918e
ahmed8047762 <an8047762@gmail.com>
2024-11-20 15:15:26 +0500

    Fix: Re-add front-end files properly

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import Student from './components/Student';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Student />
+    </div>
+  );
+}
+
+export default App;
